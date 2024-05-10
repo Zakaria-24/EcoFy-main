60e9084d962516b65d05a05a5b199f423f245b9e
Zakaria-24 <zakariajoarder.bd@gmail.com>
2024-05-11 00:36:06 +0600

    creat custom hooks

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,16 @@ import { RouterProvider } from "react-router-dom";
 import router from "./routes/Routes";
 import AuthProvider from "./providers/AuthProviders";
 import { Toaster } from "react-hot-toast";
+// TanStack query
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
       <Toaster />
     </AuthProvider>
   </React.StrictMode>
